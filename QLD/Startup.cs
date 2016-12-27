6896997c0f4cfe2170eccb5eeaf2eea3ba7fa25a
Vo Minh Quan <vominhquan.hutech@gmail.com>
2016-12-28 01:06:49 +0700

    Create Poject

---
+++ QLD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLD.Startup))]
+namespace QLD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
