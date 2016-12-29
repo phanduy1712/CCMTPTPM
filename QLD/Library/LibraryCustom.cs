2b6b2b10fa2fbff20aafef77332a7d903ea00b08
Vo Minh Quan <vominhquan.hutech@gmail.com>
2016-12-29 12:59:15 +0700

    update library

---
+++ QLD/Library/LibraryCustom.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLD.Models;
+
+namespace QLD.Library
+{
+    public class LibraryCustom
+    {
+        private  Entities db = new  Entities(); 
+           public SelectList GetStatus(int? id)
+        {
+            var list =
+            new List<SelectListItem>
+            {
+                new SelectListItem {Selected = false, Text = "Cho phép dùng", Value = "1"},
+                new SelectListItem {Selected = true, Text = "Không cho dùng", Value = "2"}
+            };
+            var obj = new SelectList(list, "Value", "Text", id);
+            return obj;
+        }
+      
+        public SelectList GetSort(int? id)
+        {
+            string[] str = new[] { "Sấp xếp", "Tăng năm sản xuất", "Giản năm sản xuất", "Tăng ngày đăng", "Giảm ngày đăng", "Tăng ngày cập nhật", "Giảm ngày cập nhật" };
+          
+            var objS = str.Select((r, index) => new   {Text = r, Value = index  });
+            var obj = new SelectList(objS, "Value", "Text", id);
+            return obj;
+        }
+      
+        public string GetLanguage()
+        {
+            string Language = "";
+            try
+            {
+                Language = HttpContext.Current.Request.Cookies["_Language"].Value;
+            }
+            catch (Exception)
+            {
+                Language = "vi-vn";
+            }
+            return Language;
+        } 
+        public bool DeleteFile(string str)
+        {
+            var fullPath = System.Web.HttpContext.Current.Server.MapPath("~" + str);
+
+            if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+              return true;
+            }
+            return false;
+        }
+    }
+    public class SubMenuBackEnd
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public Nullable<int> Id { get; set; }
+        public Nullable<int> Leve { get; set; }
+    }
+
+}
---
+++ QLD/Library/LinkHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QLD.Library
+{
+    public static class LinkHelper
+    {
+        public static MvcHtmlString EncodedActionLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, object routeValues, object htmlAttributes)
+        {
+            string queryString = string.Empty;
+            string htmlAttributesString = string.Empty;
+            if (routeValues != null)
+            {
+                RouteValueDictionary d = new RouteValueDictionary(routeValues);
+                for (int i = 0; i < d.Keys.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        queryString += "&";
+                    }
+                    if (d.Keys.ElementAt(i) == "id")
+                    {
+                        queryString += d.Keys.ElementAt(i) + "=" + HttpUtility.UrlEncode(Encrypt(d.Values.ElementAt(i).ToString()));
+                    }
+                    else
+                    {
+                        queryString += d.Keys.ElementAt(i) + "=" + d.Values.ElementAt(i);
+                    }
+                }
+            }
+            if (htmlAttributes != null)
+            {
+                RouteValueDictionary d = new RouteValueDictionary(htmlAttributes);
+                for (int i = 0; i < d.Keys.Count; i++)
+                {
+                    htmlAttributesString += " " + d.Keys.ElementAt(i) + "=" + d.Values.ElementAt(i);
+                }
+            }
+            //<a href="/Answer?questionId=14">What is Entity Framework??</a>
+            StringBuilder ancor = new StringBuilder();
+            ancor.Append("<a ");
+            if (htmlAttributesString != string.Empty)
+            {
+                ancor.Append(htmlAttributesString);
+            }
+            ancor.Append(" href='");
+            if (controllerName != string.Empty)
+            {
+                ancor.Append("/" + controllerName);
+            }
+            if (actionName != "Index")
+            {
+                ancor.Append("/" + actionName);
+            }
+            if (queryString != string.Empty)
+            {
+                ancor.Append("?" + queryString);
+            }
+            ancor.Append("'");
+            ancor.Append(">");
+            ancor.Append(linkText);
+            ancor.Append("</a>");
+            return new MvcHtmlString(ancor.ToString());
+        }
+        private static string Encrypt(string plainText)
+        {
+            string key = "jdsg432387#";
+            byte[] EncryptKey = { };
+            byte[] IV = { 55, 34, 87, 64, 87, 195, 54, 21 };
+            EncryptKey = System.Text.Encoding.UTF8.GetBytes(key.Substring(0, 8));
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByte = Encoding.UTF8.GetBytes(plainText);
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, des.CreateEncryptor(EncryptKey, IV), CryptoStreamMode.Write);
+            cStream.Write(inputByte, 0, inputByte.Length);
+            cStream.FlushFinalBlock();
+            return Convert.ToBase64String(mStream.ToArray());
+        }
+    }
+}
